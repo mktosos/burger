8774ebc15e649a268fdc5f9bde844fe04b109f24
mktosos <taniguchidesigns@hotmail.com>
2019-04-01 19:07:54 -0700

    initial file structure added

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE database burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  burger_name VARCHAR(100),
+  devoured BOOLEAN,
+  PRIMARY KEY (id)
+);
+
+SELECT * FROM burgers;
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES (cheeseburger, false);
+INSERT INTO burgers (burger_name, devoured) VALUES (whopper, false);
+INSERT INTO burgers (burger_name, devoured) VALUES (quarter pounder, false);
+INSERT INTO burgers (burger_name, devoured) VALUES (bacon cheeseburger, false);
+INSERT INTO burgers (burger_name, devoured) VALUES (double cheeseburger, false);
