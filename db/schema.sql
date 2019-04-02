72638ed03f147451c7b5cb73bcc8c30ffac01e2b
mktosos <taniguchidesigns@hotmail.com>
2019-04-01 23:40:05 -0700

    file structure finalized, server functional

--- db/schema.sql
+++ db/schema.sql
@@ -7,6 +7,7 @@ CREATE TABLE burgers (
   id INTEGER(11) AUTO_INCREMENT NOT NULL,
   burger_name VARCHAR(100),
   devoured BOOLEAN,
+  
   PRIMARY KEY (id)
 );
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
-INSERT INTO burgers (burger_name, devoured) VALUES (cheeseburger, false);
-INSERT INTO burgers (burger_name, devoured) VALUES (whopper, false);
-INSERT INTO burgers (burger_name, devoured) VALUES (quarter pounder, false);
-INSERT INTO burgers (burger_name, devoured) VALUES (bacon cheeseburger, false);
-INSERT INTO burgers (burger_name, devoured) VALUES (double cheeseburger, false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('whopper', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('quarter_pounder', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('bacon_cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('double_cheeseburger', false);
