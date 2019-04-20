01dcb58bdf1e2a9da19a535554fbf49917d44a16
mktosos <taniguchidesigns@hotmail.com>
2019-04-19 21:57:40 -0700

    connection js connection adjusted

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,22 @@
 
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3000,
-  user: "root",
-  password: "password",
-  database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "password",
+//   database: "burgers_db"
+// });
 if (process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
     host: 'localhost',
+    port: 3306,
     user: 'root',
-    password: "password",
-    database: "burgers_db"
+    password: 'password',
+    database: 'burgers_db'
   })
 }
 
