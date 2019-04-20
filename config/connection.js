f71b6483f2da61d5c806349f023bbe64bbbed28f
mktosos <taniguchidesigns@hotmail.com>
2019-04-19 19:20:19 -0700

    connection.js connection to db added

--- config/connection.js
+++ config/connection.js
@@ -8,6 +8,16 @@ var connection = mysql.createConnection({
   password: "password",
   database: "burgers_db"
 });
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'hacktheplanet',
+    database: 'todoagain_db'
+  });
+};
 
 // Make connection.
 connection.connect(function(err) {
--- db/schema.sql
+++ db/schema.sql
@@ -3,12 +3,12 @@ CREATE database burgers_db;
 
 USE burgers_db;
 
-CREATE TABLE burgers (
-  id INTEGER(11) AUTO_INCREMENT NOT NULL,
-  burger_name VARCHAR(100),
-  devoured BOOLEAN,
+-- CREATE TABLE burgers (
+--   id INTEGER(11) AUTO_INCREMENT NOT NULL,
+--   burger_name VARCHAR(100),
+--   devoured BOOLEAN,
   
-  PRIMARY KEY (id)
-);
+--   PRIMARY KEY (id)
+-- );
 
-SELECT * FROM burgers;
+-- SELECT * FROM burgers;
