fe9689215f5bf76239c75ac7a39890eeec3cf6ab
mktosos <taniguchidesigns@hotmail.com>
2019-04-19 21:34:58 -0700

    connection.js  adjustments

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
+  port: 3000,
   user: "root",
   password: "password",
   database: "burgers_db"
@@ -12,12 +12,12 @@ if (process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: 'ctgplw90pifdso61.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    user: 'zzhrq39kox9kolcr',
-    password: 'vbv8dphjixb91f',
-    database: 'vdxdraxz6knjlbai'
-  });
-};
+    host: 'localhost',
+    user: 'root',
+    password: "password",
+    database: "burgers_db"
+  })
+}
 
 // Make connection.
 connection.connect(function(err) {
